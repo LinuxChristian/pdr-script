b060c729fd1955229effa2243780268d4e46e978
Christian Brædstrup <christian@fredborg-braedstrup.dk>
2015-08-06 12:56:59 +0200

    Data is now pushed to sqlite

--- PDR_sqlite.py
+++ PDR_sqlite.py
@@ -13,19 +13,124 @@ import numpy as np
 # SQLite libs
 import sqlite3 as lite
 
+# KML libs
+from pykml import parser
+from pykml.factory import KML_ElementMaker as KML
+from lxml import etree
+
 con = None
+
+def WriteDataFile(data,poly):
+    # Write the first part
+    s = '''var exp_ChristianFredborgBrdstrupany = {
+    "type": "FeatureCollection",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    
+    "features": [
+    { "type": "Feature", "properties": {'''+'''},"Geometry":{"type",'''
+
+    # Parse polygons
+    if hasattr(place,"MultiGeometry"):
+        poly = place.MultiGeometry.Polygon;
+        s=s+'''"MultiPolygon","coordinates":[['''
+    else:
+        poly = place.Polygon;
+        s=s+'''"Polygon","coordinates":['''
+        
+    # Loop over geometries
+    for i,pol in enumerate(poly):
+        pol = str(pol.outerBoundaryIs.LinearRing.coordinates)
+        p = np.array([x.split(',') for x in (pol.split(' '))]).astype(np.float)
+        s=s+np.array_str(p).replace('\n',', ')
+
+        # Write comma if multiple polygons
+        if len(poly)-1 == i:
+            s=s+''']]}}'''
+        else:
+            s=s+'''],['''
+            
+
+    s = s+']}'
+    return s;
+
+######################
+#   Facebook Post    #
+######################
+def ParseFacebook(post):
+    name=None
+    msg=None
+    zipcode=None
+    lat=None
+    lon=None
+    image=None
+    date=None
+    town=None
+    
+    t = datetime.datetime.fromtimestamp(float(post["created_time"]))
+    date = t.strftime('%Y-%m-%d %H:%M:%S')
+    name = post["from"]["name"]
+        
+    # Grab GPS if it exists
+    if "place" in post:
+        print("Found GPS data.. haps haps haps")
+        
+    # Split message on newline
+    msg = post["message"].encode('utf-8').split('\n')
+
+    # Get zipcode
+    szipcode = [s for s in msg[0].split(" ") if (s.isdigit() and len(s)==4)]
+    zipcode = int(szipcode[0])
+    
+    # Fix combined zipcodes
+    if zipcode < 1500:
+        zipcode = "1000 - 1499"
+    elif zipcode >= 1500 and zipcode < 1800:                                        zipcode = "1500 - 1799"
+    elif zipcode >= 1800 and zipcode < 2000:                                        zipcode = "1800 - 1999"
+
+    # Validate zipcode
+    cur.execute("SELECT * FROM Zips WHERE zip == '"+str(zipcode)+"'")
+    z = cur.fetchone();
+
+    if (z is None):
+        print("Invalid zipcode!");
+    else:
+        # Get town
+        town = z[0]
+
+    return [name,date,zipcode,town,lat,lon,image]
 
 # Create the database
 def CreateDatabase(cur):
     cur.execute("CREATE TABLE Participants(Userid INT PRIMARY KEY, Name TEXT UNIQUE, Joined_on DATE)")
-    cur.execute("CREATE TABLE Beers(Beerid INT PRIMARY KEY, UserName TEXT, drank_on DATE, City TEXT, zipcode INT, Lat FLOAT, lon FLOAT, beer TEXT, image TEXT)")
+    cur.execute("CREATE TABLE Beers(Participant TEXT, Drank_on DATE, City TEXT, Zipcode INT, Image TEXT, Lat FLOAT, Lon FLOAT, Beer TEXT, CONSTRAINT unq UNIQUE (Participant, zipcode))")
 
 
-#def UpdateBeerDatabase(cur):
+def UpdateBeerDatabase(cur,
+                       e):
+    name=e[0]#.decode('unicode-escape') # Participant name
+    date=e[1] # Beer drank on date
+    city=e[3]
+    zipcode=e[2]
+    image=e[6] # Link to image
+    lat = e[4] # Latitude
+    lon = e[3] # Longitude
+    beer = None # Name of beer
+
+    # OR IGNORE
+    cur.execute('''INSERT OR IGNORE INTO Beers(Participant, Drank_on, City, Zipcode,Image) VALUES(?,?,?,?,?)''',(name,date,city,zipcode,image))
+
+    # Update with GPS
+    if lat is not None and lon is not None:
+        cur.execute('''INSERT INTO Beers(Lat,Lon) VALUES(?,?) WHERE Participant == "'''+name+'''" and Zipcode == "'''+zipcode+'''"''',(lat,lon))
+
+    # Update with beer name
+    if beer is not None:
+        cur.execute('''INSERT INTO Beers(Beer) VALUES(?) WHERE Participant == "'''+name+'''" and Zipcode == "'''+zipcode+'''"''',(beer))
 
 def UpdateUserDatabase(cur,userdb):
-    cur.execute('''INSERT OR IGNORE INTO Participants(Name) VALUES(?)''',(userdb,))
-    
+    cur.execute('''INSERT OR IGNORE INTO Participants(name) VALUES(?)''',(userdb,))
+
+
     
 # Connect to database
 try:
@@ -44,29 +149,6 @@ except lite.Error, e:
     sys.exit(1)
     
         
-# HEX transparency
-trans = "7b"
-
-# Participants: Name, Colour
-parti = [[u"Christian Fredborg Brædstrup",trans+"a6cee3"],
-         [u"Thue Sylvester Bording",trans+"1f78b4"],
-         [u"Toke Højbjerg Søltoft",trans+"b2df8a"],
-         [u"Daniel Søndergaard Skov",trans+"33a02c"],
-         [u"Niels Emil Søe",trans+"fb9a99"],
-         [u"Kenni Dinesen Petersen",trans+"e31a1c"],
-         [u"Bo Klinkvort Kempel",trans+"fdbf6f"],
-         [u"Rasmus Rumph",trans+"ff7f00"],
-         [u"Rasmus Bødker Madsen",trans+"cab2d6"],
-         [u"Jonas Møller Pedersen",trans+"6a3d9a"],
-         [u"Laust H. Lorentzen",trans+"ffff99"],
-         [u"Søren Thesbjerg Andersen",trans+"b15928"],
-         [u"Josef Nielsen",trans+"1f78b4"],
-         [u"Thomas Holm Nielsen",trans+"1f78b4"],
-         [u"Thomas L. Rasmussen",trans+"1f78b4"],
-         [u"Rasmus Bording",trans+"1f78b4"],
-         [u"Freddy TusindPenge",trans+"1f78b4"]
-]
-
 #CreateDatabase(cur)
 #for u in parti:
 #    print(u[0])
@@ -76,17 +158,15 @@ parti = [[u"Christian Fredborg Brædstrup",trans+"a6cee3"],
 #print(cur.fetchall())
 #con.commit()
 
-
-exit()
 stats = []
 
+############################
+#     FACEBOOK STUFF       #
+############################
 # First visit https://developers.facebook.com/tools/explorer for an one hour token
-token=np.loadtxt('token.txt',dtype=np.str)
+token=str(np.loadtxt('token.txt',dtype=np.str))
 
 facebookurl = 'https://graph.facebook.com/1630748980524187/feed?access_token='+token+'&since=1438372800&limit=100&date_format=U'
-
-# Note: Facebook timestamps are local to the device and encoded in Pacific time
-# Timestamps are returned in Unix epoch time
 
 print("Fetching data from Facebook")
 response = urllib2.urlopen(facebookurl)
@@ -100,77 +180,30 @@ if response.code != 200:
 jsondata = response.read()
 data = json.loads(jsondata)
 
-# Open zipcode kml file
+############################
+#    Get ZIP POLYGONS      #
+############################
 root = parser.fromstring(open('Danske_postnumre.kml','r').read())
 
 # Get Placemarks
 PM = root.Document.findall("Placemark")
 PMN = root.Document.findall("Placemark/name")
 
-# Create new empty document
-d = KML.Document(KML.name("Postnr Danmark Rundt"))
+############################
+#      UPDATE TABLE        #
+############################
+for post in data['data']:
+    if ('picture' in post) and ('www.google.com' not in post['picture']) and ('message' in post):
+        entry = ParseFacebook(post)
+        UpdateBeerDatabase(cur,entry)
+        con.commit()
+        
+############################
+#      WRITE MAP DATA      #
+############################
+    # Get data for zipcode
+#    place = PM[PMN.index(entry[2])]
+#    print(WriteDataFile(entry,place))
+#            print(p)
 
-for p in parti:
-    fld = KML.Folder(KML.name(p[0]))        
-
-    print("Processing "+p[0])
-    for post in data['data']:
-        if ('picture' in post) and ('www.google.com' not in post['picture']) and ('message' in post):
-            if (p[0] in post['from']['name']):
-                t = datetime.datetime.fromtimestamp(float(post["created_time"]))
-                tt = t.strftime('%Y-%m-%d')
-                name = post["from"]["name"].encode('utf-8')
-
-                # Grab GPS if it exists
-                if "place" in post:
-                    print("Found GPS data.. haps haps haps")
-
-                # Split message on newline
-                msg = post["message"].encode('utf-8').split('\n')
-
-                # Get zipcode
-                szipcode = [s for s in msg[0].split(" ") if (s.isdigit() and len(s)==4)]
-                zipcode = int(szipcode[0])
-
-                # Fix combined zipcodes
-                if zipcode < 1500:
-                    zipcode = "1000 - 1499"
-                elif zipcode >= 1500 and zipcode < 1800:                                        zipcode = "1500 - 1799"
-                elif zipcode >= 1800 and zipcode < 2000:                                        zipcode = "1800 - 1999"
-
-                # Get town
-                town = msg[0][5:]
-
-                place = PM[PMN.index(zipcode)]
-                # Fix a new style
-                place.styleUrl = KML.StyleUrl("#"+p[0].split(' ')[0])
-                # Add a nice comment
-                place.description = KML.description(str(tt))#+"<br> <a href='"+post["picture"]+"'>Foto</a>]]>"
-
-                fld.append(place)
-
-    # Append folder to document if any records exist
-    if (len(fld.findall('Placemark/')) > 0):
-        print("Added "+p[0])
-        stats.append([name, len(fld.findall('Placemark/')), len(fld.findall('Placemark/'))/597.0])
-        # Create unique style
-        ls = d.append(KML.Style(KML.PolyStyle(KML.LineStyle(KML.width("1.5")),KML.color(p[1])),id=p[0].split(' ')[0]))
-        d.append(fld)
-    else:
-        print("No records found")
-# Print xml pretty
-xmlstr=etree.tostring(d, pretty_print=True)
-
-# Write final string
-with open("/tmp/test.kml",'w') as f:
-    f.write(xmlstr.encode('utf-8'))
-
-
-print(stats)
-else:
-    print('Output '+output+" not supported!")
-
-if con:
-    con.close()
-
-exit()
+con.close()
